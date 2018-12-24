c4b1bbe1ed127f0407ba1ad7918a0d3257d14704
Alexander <krasnoyurchenko.a.s@gmail.com>
2018-12-24 15:13:30 +0400

    upd

--- src/components/PayForm.vue
+++ src/components/PayForm.vue
@@ -1,57 +1,96 @@
 <template lang="pug">
-	form.pay-form(action="/")
-		.card
+	form.pay-form(
+		action="/"
+		novalidate
+	)
+		.card(
+			:data-card-type="validObj.cardType"
+			:data-valid="String(checkValid())"
+		)
 			.__front
 				.__types
-
+					.__type-item(
+						v-for="typeItem in allowedCardTypes"
+						:key="typeItem.id"
+						:class="{'card__type-item--active': typeItem.name === validObj.cardType}"
+						:title="typeItem.title"
+					)
+						base-icon.__type-item-img(
+							:name="typeItem.name"
+							:width="null"
+							:height="null"
+						)
 				.__field-row
 					.__field-col
-						label.__field-label(for="number") номер карты
+						label.__field-label(for="card-number") номер карты
 
 						input.__field-input(
-							id="number"
-							name="number"
-							ref="number"
+							id="card-number"
+							name="card-number"
+							ref="cardNumber"
 							type="tel"
+							autocomplete="cc-number"
 							autofocus
+							tabindex="1"
+							@input="onCardNumberInput"
+							@keypress="onCardNumberInput"
 						)
 
 				.__field-row
 					.__field-col
-						label.__field-label(for="owner") имя владельца
+						label.__field-label(for="card-owner") имя владельца
 
 						input.__field-input(
-							id="owner"
-							name="owner"
-							ref="owner"
+							id="card-owner"
+							name="card-owner"
+							ref="cardOwner"
 							type="text"
+							autocomplete="cc-name"
+							required
+							tabindex="2"
+							@input="onCardOwnerInput"
+							@keypress="onCardOwnerInput"
 						)
 
 					.__field-col.--expiry
-						label.__field-label(for="expiry") срок действия
+						label.__field-label(for="card-expiry") срок действия
 
 						input.__field-input(
-							id="expiry"
-							name="expiry"
-							ref="expiry"
+							id="card-expiry"
+							name="card-expiry"
+							ref="cardExpiry"
 							type="tel"
+							autocomplete="cc-exp"
 							maxlength="7"
+							placeholder="ММ / ГГ"
+							tabindex="3"
+							@input="onCardExpiryInput"
+							@keypress="onCardExpiryInput"
 						)
 
 			.__back
 				.__field-row.--cvc
 					.__field-col.--cvc
-						label.__field-label(for="cvc") cvc
+						label.__field-label(for="card-cvc") cvc
 
 						input.__field-input(
-							id="cvc"
-							name="cvc"
-							ref="cvc"
+							id="card-cvc"
+							name="card-cvc"
+							ref="cardCVC"
 							type="tel"
 							autocomplete="off"
+							tabindex="4"
+							@input="onCardCVCInput"
+							@keypress="onCardCVCInput"
 						)
 
 					.__field-desc.--cvc Три цифры с&nbsp;оборотной стороны
+
+		.footer
+			.__row
+				.__col
+				.__col
+
 </template>
 
 <script>
@@ -60,13 +99,111 @@
 	export default {
 		name: 'PayForm',
 		data() {
-			return {};
+			return {
+				allowedCardTypes: [
+					{
+						id: 1,
+						name: 'mastercard',
+						title: 'MasterCard',
+					},
+					{
+						id: 2,
+						name: 'maestro',
+						title: 'Maestro',
+					},
+					{
+						id: 3,
+						name: 'visa',
+						title: 'Visa',
+					},
+				],
+				validObj: {
+					cardType: null,
+				},
+			};
 		},
 		mounted() {
-			payform.cardNumberInput(this.$refs.number);
-			payform.expiryInput(this.$refs.expiry);
-			payform.cvcInput(this.$refs.cvc);
-		}
+			payform.cardNumberInput(this.$refs.cardNumber);
+			payform.expiryInput(this.$refs.cardExpiry);
+			payform.cvcInput(this.$refs.cardCVC);
+		},
+		methods: {
+			toggleValidClass(input, status) {
+				input.classList.toggle('invalid', !status);
+			},
+			goToNextInput(currentInput) {
+				const currentTabindex = +currentInput.getAttribute('tabindex'),
+					nextInput = document.querySelector(`input[tabindex='${currentTabindex + 1}']`);
+
+				if (nextInput) {
+					nextInput.focus();
+				}
+			},
+			updateCardType(input) {
+				this.validObj.cardType = payform.parseCardType(input.value);
+				this.validateCardCVC(this.$refs.cardCVC);
+			},
+			// cardNumber
+			validateCardNumber(input) {
+				this.$set(this.validObj, 'cardNumber', payform.validateCardNumber(input.value));
+				this.toggleValidClass(input, this.validObj.cardNumber);
+			},
+			onCardNumberInput(evt) {
+				const input = evt.target;
+
+				this.updateCardType(input);
+				this.validateCardNumber(input);
+
+				if (input.value.length === 23) {
+					this.goToNextInput(input);
+				}
+			},
+			// cardOwner
+			validateCardOwner(input) {
+				this.$set(this.validObj, 'cardOwner', input.validity.valid);
+				this.toggleValidClass(input, this.validObj.cardOwner);
+			},
+			onCardOwnerInput(evt) {
+				const input = evt.target;
+
+				input.value = input.value.replace(/[^a-zA-Z-]/g, '');
+				this.validateCardOwner(input);
+			},
+			// cardExpiry
+			validateCardExpiry(input) {
+				const cardExpiryObj = payform.parseCardExpiry(input.value);
+
+				this.$set(this.validObj, 'cardExpiry', payform.validateCardExpiry(cardExpiryObj));
+				this.toggleValidClass(input, this.validObj.cardExpiry);
+			},
+			onCardExpiryInput(evt) {
+				const input = evt.target;
+
+				this.validateCardExpiry(input);
+
+				if (input.value.length === 7) {
+					this.goToNextInput(input);
+				}
+			},
+			// cardCVC
+			validateCardCVC(input) {
+				this.$set(this.validObj, 'cardCVC', payform.validateCardCVC(input.value, this.validObj.cardType));
+				this.toggleValidClass(input, this.validObj.cardCVC);
+			},
+			onCardCVCInput(evt) {
+				const input = evt.target;
+
+				this.validateCardCVC(input);
+
+				if (input.value.length === 4) {
+					this.goToNextInput(input);
+				}
+			},
+			checkValid() {
+				return Object.values(this.validObj).every(Boolean)
+					&& this.allowedCardTypes.map(typeItem => typeItem.name).indexOf(this.validObj.cardType) !== -1;
+			},
+		},
 	}
 </script>
 
@@ -100,7 +237,7 @@
 		&__front {
 			position: relative;
 			z-index: 1;
-			padding: 14px 23px 29px;
+			padding: 13px 23px 29px;
 			width: 391px;
 			height: 233px;
 			max-width: 100%;
@@ -150,10 +287,26 @@
 		}
 
 		&__types {
-			margin: 0 0 23px auto;
-			width: 154px;
-			height: 26px;
-			background: url("../assets/img/card-types.png") no-repeat 50% 50% / contain;
+			display: flex;
+			justify-content: flex-end;
+			margin: 0 -4px 13px;
+		}
+
+		&__type-item {
+			margin: 0 4px;
+			flex-shrink: 0;
+			opacity: 0.5;
+			transition: opacity 0.15s ease-in-out;
+
+			&-img {
+				display: block;
+				width: auto;
+				height: 36px;
+			}
+
+			&--active {
+				opacity: 1;
+			}
 		}
 
 		&__field {
@@ -220,14 +373,17 @@
 
 				padding: (40px - 2px - 14px * 1.25) / 2 10px;
 				width: 100%;
+				height: 40px;
 				font-size: 14px;
 				line-height: 1.25;
 				border-radius: 4px;
 				border: 1px solid #cccccc;
 				background-color: #ffffff;
-				transition-property: color,
-				border-color,
-				background-color;
+				transition-property:
+					color,
+					border-color,
+					background-color,
+					box-shadow;
 				transition-duration: 0.15s;
 				transition-timing-function: ease-in-out;
 
@@ -239,12 +395,19 @@
 				&:focus-visible {
 					outline: 0;
 					border-color: #b49ed6;
+					box-shadow: 0 0 3px rgba(#b49ed6, 0.35);
+				}
+
+				&.invalid {
+					border-color: red;
+					box-shadow: 0 0 3px rgba(red, 0.35);
 				}
 			}
 
 			&-desc {
+				@include reset;
+
 				color: #999999;
-				font-family: var(--content-font-family);
 				font-size: 12px;
 				line-height: 1.25;
 				user-select: none;
